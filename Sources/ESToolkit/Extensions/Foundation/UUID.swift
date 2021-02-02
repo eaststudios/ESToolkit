8cd5a092984b5552586e52f823a1c7d2f021c42b
Oliver Kulpakko <oliver@kulpakko.com>
2021-02-02 17:39:19 +0200

    UUID extension

---
+++ Sources/ESToolkit/Extensions/Foundation/UUID.swift
@@ -0,0 +1,12 @@
+//
+//  UUID.swift
+//
+//  Created by Oliver Kulpakko on 2.2.2021.
+//  Copyright © 2021 East Studios Oy. All rights reserved.
+//
+
+import Foundation
+
+public extension UUID {
+	public static let zero = UUID(uuidString: "00000000-0000-0000-0000-000000000000")!
+}

9bc505e93e13c39d422c4607aa079a5121c996c7
Oliver Kulpakko <oliver@kulpakko.com>
2021-02-02 17:41:54 +0200

    UIBarButtonItem extension

---
+++ Sources/ESToolkit/Extensions/UIKit/UIBarButtonItem.swift
@@ -0,0 +1,12 @@
+//
+//  UIBarButtonItem.swift
+//
+//  Created by Oliver Kulpakko on 2.2.2021.
+//  Copyright © 2021 East Studios Oy. All rights reserved.
+//
+
+import UIKit
+
+public extension UIBarButtonItem {
+	public static let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+}

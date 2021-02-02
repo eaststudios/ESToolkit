e75bdb69cb8550fe7c78bbae2a4c8ea219032251
Oliver Kulpakko <oliver@kulpakko.com>
2021-02-02 17:23:01 +0200

    Add min platform

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "ESToolkit",
-    products: [
+	platforms: [
+		.iOS(.v13),
+	],
+	products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ESToolkit",
